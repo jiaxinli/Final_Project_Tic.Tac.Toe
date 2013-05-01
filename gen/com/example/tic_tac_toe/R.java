fe4d280e1951e487f345732f819c143532390f8a
Jiaxin Li <jiaxinli@smu.edu>
2013-04-30 21:47:10 -0500

    craete enter page

--- gen/com/example/tic_tac_toe/R.java
+++ gen/com/example/tic_tac_toe/R.java
@@ -22,6 +22,8 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int logo_text=0x7f020001;
+        public static final int tic_tac_toe=0x7f020002;
     }
     public static final class id {
         public static final int action_settings=0x7f080000;
@@ -35,7 +37,12 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int description=0x7f050002;
+        public static final int description2=0x7f050003;
+        public static final int description3=0x7f050004;
+        public static final int description4=0x7f050005;
+        public static final int description5=0x7f050006;
+        public static final int description6=0x7f050007;
     }
     public static final class style {
         /** 
