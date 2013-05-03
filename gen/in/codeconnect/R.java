14d626c5f710af430bdf19061b92ecdd3b4feea2
Jiaxin Li <jiaxinli@smu.edu>
2013-05-02 23:25:42 -0500

    add a new look of start page

--- gen/in/codeconnect/R.java
+++ gen/in/codeconnect/R.java
@@ -10,29 +10,54 @@ package in.codeconnect;
 public final class R {
     public static final class attr {
     }
+    public static final class dimen {
+        /**  Default screen margins, per the Android Design guidelines. 
+
+         Customize dimensions originally defined in res/values/dimens.xml (such as
+         screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
+    
+         */
+        public static final int activity_horizontal_margin=0x7f040000;
+        public static final int activity_vertical_margin=0x7f040001;
+    }
     public static final class drawable {
         public static final int icon=0x7f020000;
-        public static final int o=0x7f020001;
-        public static final int x=0x7f020002;
-        public static final int xo=0x7f020003;
+        public static final int logo_text=0x7f020001;
+        public static final int o=0x7f020002;
+        public static final int tic_tac_toe=0x7f020003;
+        public static final int x=0x7f020004;
+        public static final int xo=0x7f020005;
     }
     public static final class id {
-        public static final int btn1=0x7f050000;
-        public static final int btn2=0x7f050001;
-        public static final int btn3=0x7f050002;
-        public static final int btn4=0x7f050003;
-        public static final int btn5=0x7f050004;
-        public static final int btn6=0x7f050005;
-        public static final int btn7=0x7f050006;
-        public static final int btn8=0x7f050007;
-        public static final int btn9=0x7f050008;
-        public static final int subtitle=0x7f050009;
+        public static final int action_settings=0x7f07000a;
+        public static final int btn1=0x7f070000;
+        public static final int btn2=0x7f070001;
+        public static final int btn3=0x7f070002;
+        public static final int btn4=0x7f070003;
+        public static final int btn5=0x7f070004;
+        public static final int btn6=0x7f070005;
+        public static final int btn7=0x7f070006;
+        public static final int btn8=0x7f070007;
+        public static final int btn9=0x7f070008;
+        public static final int subtitle=0x7f070009;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int activity_start_page=0x7f030000;
+        public static final int main=0x7f030001;
+    }
+    public static final class menu {
+        public static final int start_page=0x7f060000;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int action_settings=0x7f050003;
+        public static final int app_name=0x7f050001;
+        public static final int description=0x7f050004;
+        public static final int description2=0x7f050005;
+        public static final int description3=0x7f050006;
+        public static final int description4=0x7f050007;
+        public static final int description5=0x7f050008;
+        public static final int description6=0x7f050009;
+        public static final int hello=0x7f050000;
+        public static final int title_activity_start_page=0x7f050002;
     }
 }
